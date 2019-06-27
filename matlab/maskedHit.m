dc542b46b2628e57706ea1166ef83b47e43542cc
Eduardo R. Cruz-Chu <eduardo@nadie.pe>
2019-06-27 11:22:48 -0500

    Create maskedHit.m

---
+++ matlab/maskedHit.m
@@ -0,0 +1,28 @@
+function maskedHit( inFile , outFile )
+
+% read data
+inMatrix = load(inFile);
+
+% load pixels
+y = inMatrix.y;
+
+% load mask
+mask = inMatrix.yMask;
+maskBool=strcmp(mask, 'FALSE');
+
+% select area of interest
+yGoodPixels=y(maskBool,:);
+
+% clear variables
+clear y;
+clear mask;
+clear maskBool;
+
+y = yGoodPixels;
+
+clear yGoodPixels;
+
+
+% outpu data
+save(outFile,'y','-v7.3');
+
